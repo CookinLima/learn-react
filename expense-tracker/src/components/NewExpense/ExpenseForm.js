e2a92495d82bdde776b03da24ba4e23898230b25
Kenny Wong <kken.w@hotmail.com>
2022-06-28 23:21:33 +0800

    created submitEvent for form on ExpenseForm and erase input on form on submit

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -7,23 +7,38 @@ const ExpenseForm = () => {
   const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (e) => {
+    // console.log(typeof enteredTitle);
     setEnteredTitle(e.target.value);
-    console.log(setEnteredTitle);
   };
   const amountChangeHandler = (e) => {
     setEnteredAmount(e.target.value);
-    console.log(e.target.value);
   };
   const dateChangeHandler = (e) => {
-    setEnteredDate(e.taraget.value);
-    console.log(e.target.value);
+    setEnteredDate(e.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -31,6 +46,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -40,6 +56,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
