ccd5d25006fe1732e3a9cdf5203611a87c7744ee
Kenny Wong <kken.w@hotmail.com>
2022-06-29 11:40:12 +0800

    added filter component

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "../ExpenseFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   // console.log(props.allExpenses);
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const saveFilterExpenseData = (selectedYear) => {
+    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        setYear={filteredYear}
+        onFilterExpenseData={saveFilterExpenseData}
+      />
       {props.allExpenses.map((item) => {
         // console.log(item);
         return (
@@ -21,26 +32,3 @@ function Expenses(props) {
   );
 }
 export default Expenses;
-
-{
-  /* <ExpenseItem
-        title={props.allExpenses[0].title}
-        amount={props.allExpenses[0].amount}
-        date={props.allExpenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.allExpenses[1].title}
-        amount={props.allExpenses[1].amount}
-        date={props.allExpenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.allExpenses[2].title}
-        amount={props.allExpenses[2].amount}
-        date={props.allExpenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.allExpenses[3].title}
-        amount={props.allExpenses[3].amount}
-        date={props.allExpenses[3].date}
-      ></ExpenseItem> */
-}
